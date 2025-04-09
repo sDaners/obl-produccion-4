ac3702fc31fdd1642dff944178cd6be319f967af
lad <lad@facultades.ort.edu.uy>
2025-04-09 20:47:54 -0300

    Fixed projectile, removed plugins

--- Plugins/GitSourceControl/Source/GitSourceControl/Private/GitSourceControlChangelist.cpp
+++
@@ -1,6 +0,0 @@
-#include "GitSourceControlChangelist.h"
-
-#if ENGINE_MAJOR_VERSION == 5
-FGitSourceControlChangelist FGitSourceControlChangelist::WorkingChangelist(TEXT("Working"), true);
-FGitSourceControlChangelist FGitSourceControlChangelist::StagedChangelist(TEXT("Staged"), true);
-#endif
